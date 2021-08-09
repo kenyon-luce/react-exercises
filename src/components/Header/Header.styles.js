7b98dcbc99fcd9bdecc500be3117751c1c748313
Kenyon Luce <kenyon.luce02@gmail.com>
2021-08-09 17:01:35 -0500

    setting up header

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,10 @@
+//setting up placeholder elements
+import styled from 'styled-components';
+
+export const Wrapper = styled.div``;
+
+export const Content = styled.div``;
+
+export const LogoImg = styled.img``;
+
+export const TMDBLogoImg = styled.img``;
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+//importing placeholder elements
+//LOGOS
+import RMDBLogo from '../../images/react-movie-logo.svg';
+import TMDBLogo from '../../images/tmdb_logo.svg';
+//STYLES
+import {Wrapper, Content, LogoImg, TMDBLogoImg} from './Header.styles';
+//the "{}" lets us import more than one element from the same component
+
+const Header = () => (
+    <Wrapper>
+        <Content>
+            <LogoImg src={RMDBLogo} alt='rmdb-logo'/>
+            <TMDBLogoImg src={TMDBLogoImg} alt='tmdb-logo'/>
+        </Content>
+    </Wrapper>
+);
+
+export default Header;
