838d3929f786f02ee91c54c73f8926be6eeb209c
Kenyon Luce <kenyon.luce02@gmail.com>
2021-08-10 15:16:35 -0500

    added and styled header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import Header from './components/Header';
+
 // import styled from 'styled-components'
 import {GlobalStyle} from './GlobalStyle'
 //^^here, we imported a global version of the line above it, allowing us to call on and use this styling outside of App.js
@@ -10,6 +12,7 @@ function App() {
   return (
       //this is an "App" component, as defined below
     <div className="App">
+        <Header/>
       Start here.
         <GlobalStyle/>
     {/*  ^^^  brought in the styling within the div*/}
--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,10 +1,34 @@
 //setting up placeholder elements
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+//creating space for content
+export const Wrapper = styled.div`
+    background: var(--darkGrey);
+    padding: 0 30px;
+`;
 
-export const Content = styled.div``;
+//organized content within the wrapper
+export const Content = styled.div`
+    display: flex;
+    align-items:center;
+    justify-content: space-between;
+    max-width: ver(--maxWidth);
+    padding: 20px 0;
+    margin: 0 auto;
+`;
 
-export const LogoImg = styled.img``;
-
-export const TMDBLogoImg = styled.img``;
+//QUESTION: where does it know what image is what for the below instances?
+export const LogoImg = styled.img`
+    width: 200px;
+    
+    // responsive design
+    @media screen and (max-width: 500px) {
+        width 150px;
+    }
+`;
+export const TMDBLogoImg = styled.img`
+    width: 100px;
+    @media screen and (max-width: 500px) {
+        width: 80px;
+    }
+`;
