0329f4316d638dad042fb10edbfa9bd818345e6f
Kenyon Luce <kenyon.luce02@gmail.com>
2021-08-14 21:20:05 -0500

    styled thumbnails

--- src/components/Thumb/Thumb.styles.js
+++ src/components/Thumb/Thumb.styles.js
@@ -1,5 +1,22 @@
 import styled from "styled-components";
 
 export const Image = styled.img`
-
+    width: 100%;
+    max-width: 720px;
+    transition: all 0.3s;
+    object-fit: cover; //centers image to fit within thumbnails
+    border-radius: 20px;
+    animation: animateThumb 0.5s;
+    
+    :hover {
+        opacity: 0.8; //makes image lighter on hover
+    }
+    
+    @keyframes animateThumb {
+        from {
+            opacity: 0;
+        } to {
+            opacity: 1;
+        }
+    }
 `;
