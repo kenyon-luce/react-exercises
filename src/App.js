6dd38e99e9c63b863f9b4c42ec53d33c1221436c
Kenyon Luce <kenyon.luce02@gmail.com>
2021-08-20 16:54:39 -0500

    adding routers, connection issue

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import Header from './components/Header';
 import Home from './components/Home';
 
-// import styled from 'styled-components'
+//Styling
 import {GlobalStyle} from './GlobalStyle'
-//^^here, we imported a global version of the line above it, allowing us to call on and use this styling outside of App.js
-
 //Routing
 import {BrowserRouter as Router, Routes, Route} from 'react-router'
 
-function App() {
-  return (
-      //this is an "App" component, as defined below
-    <div className="App">
+const App = () = (
+    <Router>
         <Header/>
-        <Home/>
-      {/*Start here.*/}
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+        </Routes>
         <GlobalStyle/>
-    {/*  ^^^  brought in the styling within the div*/}
-    </div>
-  );
-}
-
-////es6 syntax
-// const Star = () => React.createElement('div', null, 'This is a little star');
-// const App = () => {
-//     return Star();
-// }
+    </Router>
+);
 
 export default App;
