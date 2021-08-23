f177c935641bfede27381750733c8dbebe43f36d
Kenyon Luce <kenyon.luce02@gmail.com>
2021-08-23 15:24:54 -0500

    implementing routes workaround

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import React from 'react';
+
+//Routing
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//Components
 import Header from './components/Header';
 import Home from './components/Home';
 
 //Styling
 import {GlobalStyle} from './GlobalStyle'
-//Routing
-import {BrowserRouter as Router, Routes, Route} from 'react-router'
 
-const App = () = (
+const App = () => (
     <Router>
         <Header/>
-        <Routes>
-            <Route path='/' element={<Home/>}/>
-        </Routes>
+        {/*<Routes>*/}
+        {/*    <Route path='/' element={<Home/>}/>*/}
+        {/*    <Route path='/:movieId' element={<Movie/>}/>*/}
+        {/*</Routes>*/}
+        {/*^^since importing Routes isn't working, I will just do each individual Route*/}
+        <Route path='/'>
+            <Home/>
+        </Route>
+        <Route path='/:movie'>
+            {/*<Movie/>*/}
+        </Route>
         <GlobalStyle/>
     </Router>
 );
