25a1f405e417026396dac8913dc74150064bb9af
Kenyon Luce <kenyon.luce02@gmail.com>
2021-08-26 15:18:28 -0500

    setting up movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import {IMAGE_BASE_URL, POSTER_SIZE} from "../config";
+import Grid from 'Grid';
+import Spinner from './Spinner';
+import NoImage from '../images/no_image.jpg';
 
-const Movie = () => <div>Movie</div>
+const Movie = () => { //using brackets because we want to include logic for fetching info from the API
+
+    return (
+        //fragment hehehe
+        <>
+            <div>Movie</div>
+        </>
+    )
+
+}
 
 export default Movie;
