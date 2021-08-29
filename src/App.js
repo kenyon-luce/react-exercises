a1e7e0545d54382946ca43af485a7fafb0fd457f
Kenyon Luce <kenyon.luce02@gmail.com>
2021-08-28 21:08:48 -0500

    refactored routes and retrieving movie info from api

--- src/App.js
+++ src/App.js
@@ -16,23 +16,22 @@ import {GlobalStyle} from './GlobalStyle'
 const App = () => (
     <Router>
         <Header/>
-        {/*<Routes>*/}
-        {/*    <Route path='/' element={<Home/>}/>*/}
-        {/*    <Route path='/:movieId' element={<Movie/>}/>*/}
-        {/*</Routes>*/}
-        {/*^^since Routes cannot be used in my case, I will use Switch instead*/}
         <Routes>
-            <Route exact path='/'>
-                <Home/>
-            </Route>
-            <Route exact path='/:movie'>
-                <Movie/>
-            </Route>
-            <Route>
-                <NotFound/>
-            </Route>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/:movieId' element={<Movie/>}/>
+            <Route path='/*' element={<NotFound/>}/>
         </Routes>
-
+        {/*<Switch>*/}
+        {/*    <Route exact path='/'>*/}
+        {/*        <Home/>*/}
+        {/*    </Route>*/}
+        {/*    <Route exact path='/:movie'>*/}
+        {/*        <Movie/>*/}
+        {/*    </Route>*/}
+        {/*    <Route>*/}
+        {/*        <NotFound/>*/}
+        {/*    </Route>*/}
+        {/*</Switch>*/}
         <GlobalStyle/>
     </Router>
 );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
-import {IMAGE_BASE_URL, POSTER_SIZE} from "../config";
+import {API_KEY, IMAGE_BASE_URL, POSTER_SIZE} from "../config";
 import Grid from './Grid';
 import Spinner from './Spinner';
 import NoImage from '../images/no_image.jpg';
@@ -11,7 +11,7 @@ const Movie = () => { //using brackets because we want to include logic for fetc
 
     const {state: movie, loading, error} = useMovieFetch(movieId);
     //you can change the name of a property using ':'
-    console.log(movieId);
+    console.log(movie);
     return (
         <>
             <div>Movie</div>
