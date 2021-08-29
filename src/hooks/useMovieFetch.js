ec5208869e2b2b3c85e4b6abb1af8a1b1a0b4d6a
Kenyon Luce <kenyon.luce02@gmail.com>
2021-08-28 21:37:24 -0500

    refactored movie data fetch

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useCallback} from 'react';
 import API from '../API';
 
 export const useMovieFetch = movieId => {
@@ -6,32 +6,33 @@ export const useMovieFetch = movieId => {
     const [loading, setLoading] = useState({});
     const [error, setError] = useState({});
 
+    const fetchMovie = useCallback(async () => {
+        try {
+            setLoading(true);
+            setError(false);
+
+            const movie = await API.fetchMovies(movieId);
+            const credits = await API.fetchCredits(movieId);
+            //Directors only
+            const directors = credits.crew.filter(
+                member => member.job === 'Director'
+            );
+            //exporting state object
+            setState({
+                ...movie,
+                actors: credits.cast,
+                directors
+            });
+
+            setLoading(false);
+        } catch (error) {
+            setError(true);
+        }
+    }, [movieId]);
+
     useEffect(() => {
-        const fetchMovie = async () => {
-            try {
-                setLoading(true);
-                setError(false);
-
-                const movie = await API.fetchMovies(movieId);
-                const credits = await API.fetchCredits(movieId);
-                //Directors only
-                const directors = credits.crew.filter(
-                    member => member.job === 'Director'
-                );
-                //exporting state object
-                setState({
-                    ...movie,
-                    actors: credits.cast,
-                    directors
-                });
-
-                setLoading(false);
-            } catch (error) {
-                setError(true);
-            }
-        };
-        fetchMovie();
-    }, [movieId])
+        fetchMovie(); //fetch movie is an anonymous function, so it can be called on
+    }, [movieId, fetchMovie])
 
     return {state, loading, error};
 }
