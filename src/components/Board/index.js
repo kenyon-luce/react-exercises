fef86663cf75aaa6bc6d2463f1ea046c447819ed
Kenyon Luce <kenyon.luce02@gmail.com>
2021-09-09 21:20:45 -0500

    saving changes

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,12 +1,4 @@
-import bodyParser from "body-parser";
 import React from "react";
 import {BoardGrid} from './Board.styles';
-
-const Board = () => (
-    <body>
-        
-    </body>
-)
-
 
 export default BoardGrid;
