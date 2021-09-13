58255a5900c17e6abb3605845c04a99511b92861
Kenyon Luce <kenyon.luce02@gmail.com>
2021-09-13 18:51:48 -0500

    attempting to create inline grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import React from 'react';
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
 //Components
-import Board from "./components/Board"
-import {Square} from "./components/Square";
+// import Board from "./components/Board"
+// import {Square} from "./components/Square";
+
+// const Square = styled.div`
+// display: grid;
+//         grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); //each column is 200px and it fits as many columns as it can depending on the page width
+//         grid-gap: 2rem;
+//         background: #fff;
+//         border: 1px solid #999;
+//         float: left;
+//         font-size: 24px;
+//         font-weight: bold;
+//         line-height: 34px;
+//         height: 34px;
+//         margin-right: -1px;
+//         margin-top: -1px;
+//         padding: 0;
+//         text-align: center;
+//         width: 34px;
+// `;
+
+const Grid = styled.div`
+max-width: var(--maxWidth);
+margin: 0 auto;
+padding: 0 20px;
+`;
+
+const Square = styled.div`
+// background: #fff;
+        border: 1px solid #999;
+//         // float: left;
+//         font-size: 24px;
+//         font-weight: bold;
+//         line-height: 34px;
+//         height: 34px;
+//         // margin-right: -1px;
+//         // margin-top: -1px;
+//         padding: 0;
+//         // text-align: center;
+//         width: 34px;
+    display: grid;
+    //fragments?
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); //each column is 200px and it fits as many columns as it can depending on the page width
+    grid-gap: 2rem;
+`;
+
+const Board = () => (
+  <Grid>
+    <Square/>
+  </Grid>
+)
+
 
 function App() {
   return (
@@ -11,6 +61,7 @@ function App() {
       <div className="App">
         Start here.
       </div>
+      {/* <Square/> */}
       <Board/>
     </>
   );
